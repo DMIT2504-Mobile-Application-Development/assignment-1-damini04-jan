d1b25650f74a678d4eb12b73e758e071abcab524
damini04-jan <d2245045@gmail.com>
2025-05-21 18:43:55 -0600

    Add CurrentWeather model with private fields and public  getters

---
+++ models/current_weather.dart
@@ -0,0 +1,14 @@
+class CurrentWeather {
+  String _city;
+  String _description;
+  double _currentTemp;
+  DateTime _currentTime;
+  DateTime _sunrise;
+  DateTime _sunset;
+  // geters
+  String get city => _city;
+  String get description => _description;
+  double get currentTemp => _currentTemp;
+  DateTime get currentTime => _currentTime;
+  DateTime get sunrise => _sunrise;
+  DateTime get sunset => _sunset;
