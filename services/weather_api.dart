37715d05b8a0458a3228d564f0624ca0b9e15a5d
damini04-jan <d2245045@gmail.com>
2025-05-21 19:24:23 -0600

    add weather API key and endpoint

--- services/weather_api.dart
+++ services/weather_api.dart
@@ -1,2 +1,5 @@
 import 'dart:convert';
 import 'package:http/http.dart' as http;
+
+const String weatherApiKey = 'c4acb9b14827de37ff4dc75f8a07bd4c';
+const String currentWeatherEndpoint = 'https://api.openweathermap.org/data/2.5/weather';
