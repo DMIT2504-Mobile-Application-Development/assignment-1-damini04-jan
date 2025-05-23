657d4fa32293e57882f752f6bcd931f6ce478e06
damini04-jan <d2245045@gmail.com>
2025-05-22 21:36:01 -0600

    add method to fetch weather data

--- services/weather_api.dart
+++ services/weather_api.dart
@@ -3,3 +3,7 @@ import 'package:http/http.dart' as http;
 
 const String weatherApiKey = 'c4acb9b14827de37ff4dc75f8a07bd4c';
 const String currentWeatherEndpoint = 'https://api.openweathermap.org/data/2.5/weather';
+
+Future<dynamic> getWeatherForCity({required String city}) async {
+  final url = Uri.parse('$currentWeatherEndpoint?units=metric&q=$city&appid=$weatherApiKey');
+  //print('[DEBUG ] URL= $url');
